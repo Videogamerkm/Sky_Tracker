2752a3fa9b59fe60f154a2aae7b666cacd98556b
Videogamerkm <videogamerkm@gmail.com>
2024-03-12 23:56:44 -0400

    Resolve #56
    Huge tree improvements. Moved Back, Buy all, and Clear buttons into the trees. May move the actual code to do everything to do with the tree into the tree, but that would be slightly more difficult.

--- friendship_tree.gd
+++ friendship_tree.gd
@@ -1,14 +1,16 @@
 extends VBoxContainer
 
-@onready var iconRow = $"Icon Row".duplicate()
-@onready var lineRow = $"Line Row".duplicate()
+@onready var iconRow = %Rows/"Icon Row".duplicate()
+@onready var lineRow =  %Rows/"Line Row".duplicate()
 
 signal bought(iconValue,press)
 signal reject
+signal treeBack
+signal treeClear
 
 func set_tree(rows,days=""):
-	for c in get_children():
-		remove_child(c)
+	for c in %Rows.get_children():
+		%Rows.remove_child(c)
 		c.queue_free()
 	var x = 0
 	for r in rows:
@@ -31,49 +33,50 @@ func set_tree(rows,days=""):
 				icons.get_child(i).type = "0"
 				icons.get_child(i).hide()
 				lines.get_child(i).hide()
-		add_child(icons)
-		add_child(lines)
+		%Rows.add_child(icons)
+		%Rows.add_child(lines)
 		x += 1
-	remove_child(get_child(-1))
-	get_child(-1).get_child(1).set_locked(false)
+	custom_minimum_size = Vector2(0,x*100-25)
+	%Rows.remove_child(%Rows.get_child(-1))
+	%Rows.get_child(-1).get_child(1).set_locked(false)
 
 func icon_pressed(button_pressed,node,row):
 	if button_pressed:
 		row -= 1
 		if row >= 0 && node.name == "Mid":
 			for i in range(0,3):
-				get_child(row*2).get_child(i).set_locked(false)
-		if row >= -1 && row < get_children().size() / 2.0:
-			for r in range(row+2,get_children().size()/2.0 + 1):
-				get_child(r*2).get_child(1).set_locked(false)
-				get_child(r*2).get_child(1).set_pressed(true)
+				%Rows.get_child(row*2).get_child(i).set_locked(false)
+		if row >= -1 && row < %Rows.get_children().size() / 2.0:
+			for r in range(row+2,%Rows.get_children().size()/2.0 + 1):
+				%Rows.get_child(r*2).get_child(1).set_locked(false)
+				%Rows.get_child(r*2).get_child(1).set_pressed(true)
 	else:
 		if row >= 1 && node.name == "Mid":
 			for r in range(0,row):
 				for i in range(0,3):
-					get_child(r*2).get_child(i).set_locked(true)
-					get_child(r*2).get_child(i).set_pressed(false)
+					%Rows.get_child(r*2).get_child(i).set_locked(true)
+					%Rows.get_child(r*2).get_child(i).set_pressed(false)
 	bought.emit(node.iconValue,button_pressed)
 
 func import_bought(vals: Array):
-	if 2 * vals.size() - 1 != get_child_count():
+	if 2 * vals.size() - 1 != %Rows.get_child_count():
 		reject.emit()
 		return
 	var c = 0
 	for row in vals:
 		var r = 0
 		for b in row:
-			if (b and not get_child(c).get_child(r).is_visible()) or (b == null and get_child(c).get_child(r).is_visible()):
+			if (b and not %Rows.get_child(c).get_child(r).is_visible()) or (b == null and %Rows.get_child(c).get_child(r).is_visible()):
 				reject.emit()
 				return
-			if b != null: get_child(c).get_child(r).set_pressed(b)
+			if b != null: %Rows.get_child(c).get_child(r).set_pressed(b)
 			r += 1
 		c += 2
 
 func export_bought() -> Array:
 	var ret = []
 	var c = 0
-	for child in get_children():
+	for child in %Rows.get_children():
 		if c % 2 == 0:
 			var row = []
 			for i in range(0,3):
@@ -86,7 +89,7 @@ func export_bought() -> Array:
 func buy_all() -> Array:
 	var ret = []
 	var c = 0
-	for child in get_children():
+	for child in %Rows.get_children():
 		if c % 2 == 0:
 			var row = []
 			for i in range(0,3):
@@ -97,3 +100,12 @@ func buy_all() -> Array:
 			ret.append(row)
 		c += 1
 	return ret
+
+func _on_back_pressed():
+	treeBack.emit()
+
+func _on_clear_pressed():
+	$Confirm.show()
+
+func _on_confirm_confirmed():
+	treeClear.emit()
